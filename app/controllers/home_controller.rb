c30cbfb3734f2c9057c2f9ef9f58e92c37f337c3
unknown <albertkantwi@gmail.com>
2024-01-27 22:51:40 +0000

    Add controllers

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+    def index
+      @search_logs = SearchLog.order(created_at: :desc).limit(10)
+    end
+  
+    def search
+      query = params[:query]
+      ip_address = request.remote_ip
+      user = current_user || User.create(name: "Guest")  # Create a guest user if not logged in
+      SearchLog.create(user: user, query: query, ip_address: ip_address)
+      ActionCable.server.broadcast("search_channel", { html: render_to_string(partial: 'search_log', locals: { search_log: SearchLog.new(user: user, query: query, ip_address: ip_address) }) })
+    end
+  end
+  
